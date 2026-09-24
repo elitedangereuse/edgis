8468e748c1e0ee8fe5d3e42789f43e134c9c0221
ThArGos <thargos@gmail.com>
2026-09-24 17:34:54 +0200

    fix: mega ship icon

--- api/static/js/sysmap_roots.js
+++ api/static/js/sysmap_roots.js
@@ -85,7 +85,7 @@ function stationIconAsset(stationType){
     if(type.includes('coriolis')) return 'coriolisstation.svg';
     if(type.includes('orbis')) return 'orbisstation.svg';
     if(type.includes('outpost')) return 'outpoststation.svg';
-    if(type.includes('megaship')) return 'megashipstation.svg';
+    if(type.includes('mega')) return 'megashipstation.svg';
     if(type.includes('squadron')) return 'squadroncarrierstation.svg';
     if(type.includes('installation')) return 'installationstation.svg';
     if(type.includes('disabledfleetfixme')) return 'disabledfleetcarrierstation.svg';
